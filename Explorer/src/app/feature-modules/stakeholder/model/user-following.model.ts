0f5f9eb623860c57014464d0101247bde941b8ca
Anja Ducic <ducic.ra5.2020@uns.ac.rs>
2024-04-16 21:18:37 +0200

    feat: Following model for neo4j

---
+++ Explorer/src/app/feature-modules/stakeholder/model/user-following.model.ts
@@ -0,0 +1,8 @@
+export interface UserFollowing {
+    userId: string;
+    username: string;
+    profilemage: string;
+    followingUserId: string;
+    followingUsername: string;
+    followingProfileImage: string;
+}
